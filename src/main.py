f5aba135224ad91936d2e13b6a892c9aaaba1908
Dhruva Chayapathy <dhruva.chayapathy@gmail.com>
2022-04-06 14:44:44 -0400

    pretty close

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import math
+import numpy as np
 import cv2
 import threading
 class cameras(threading.Thread):
@@ -12,12 +14,8 @@ class cameras(threading.Thread):
 		while True:
 			self.ret,self.frame = self.cam.read()
 
-	def display(self):
-		cv2.imshow("camera "+str(self.camID),self.frame)
-		key = cv2.waitKey(0)
-		if key== ord('q'):
-			cv2.destroyAllWindows()
-			exit(1)
+	def get(self):
+		return self.frame
 def listCam():
 	index=0
 	valid_cams = []
@@ -33,6 +31,22 @@ def listCam():
 				valid_cams.append(index)
 		index+=1
 	return valid_cams
+def combine(cameraList):
+	dx = int(1000/math.sqrt(len(cameraList)))
+	dy = int(1000/(len(cameraList)/int(math.sqrt(len(cameraList)))))
+	print(dy)
+	img = [cv2.resize(cameraList[0].get(),(dx,dy),interpolation=cv2.INTER_AREA)]
+	for i,camera in enumerate(cameraList):
+		if(i==0):
+			continue
+		img[i%int(math.sqrt(len(cameraList)))] = np.concatenate((img[i%int(math.sqrt(len(cameraList)))],cv2.resize(camera.get(),(dx,dy),interpolation=cv2.INTER_AREA)),axis=0)
+	finalImg=img[0]
+	for i,image in enumerate(img):
+		if(i==0):
+			continue
+		finalImg = np.concatenate((finalImg,image),axis=1)
+	cv2.imshow("total",finalImg)
+	cv2.waitKey(1)
 def main():
 	allCams = listCam()
 	threads = []
@@ -40,12 +54,10 @@ def main():
 		threads.append(cameras(num,"camera "+str(num),i))
 		threads[i].start()
 	while True:
-		for j,i in enumerate(threads):
-			try:
-				i.display()
-			except AttributeError:
-				pass
-		
+		try:
+			combine(threads)
+		except AttributeError:
+			pass
 	for i in threads:
 		i.join()
 if __name__=="__main__":
